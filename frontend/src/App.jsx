0706c4c5bce0d5579a27d5fb3474746e06f42497
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-05-31 14:08:12 +0530

    hero section

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className=''>
+    <Navbar />
+    <Hero />
+    <Highlights />
+    </div>
+  )
+}
+
+export default App
