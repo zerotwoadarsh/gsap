1bdd372e29e54a076a1a9bade6fe718d135937b5
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-02 00:53:53 +0530

    features

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -44,6 +44,13 @@ const Features = () => {
             <h2 className="text-5xl lg:text-7xl font-semibold">Forged in titanium.</h2>
           </div>
 
+          <div className="flex-center flex-col sm:px-10">
+            <div className="relative h-[50vh] w-full flex items-center">
+              <video playsInline id="exploreVideo" className="w-full h-full object-cover object-center" preload="none" muted autoPlay ref={videoRef}>
+                <source src={exploreVideo} type="video/mp4" />
+              </video>
+            </div>
+
 
 
 
