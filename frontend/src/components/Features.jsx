d12c61521462f422425b0d7a83a125ace99243b8
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-02 00:56:01 +0530

    features

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -72,6 +72,16 @@ const Features = () => {
                   </p>
                 </div>
 
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of any metal, making these our {' '}
+                    <span className="text-white">
+                      lightest Pro models ever.
+                    </span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+
 
           )
 }
