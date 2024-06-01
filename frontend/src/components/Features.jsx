e5e67a7ebb363a4546b339256ffc63bac02cd675
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-02 00:49:37 +0530

    features

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -3,3 +3,18 @@ import React, { useRef } from 'react'
 import { animateWithGsap } from '../utils/animations';
 import { explore1Img, explore2Img, exploreVideo } from '../utils';
 import gsap from 'gsap';
+
+const Features = () => {
+  const videoRef = useRef();
+
+  useGSAP(() => {
+    gsap.to('#exploreVideo', {
+      scrollTrigger: {
+        trigger: '#exploreVideo',
+        toggleActions: 'play pause reverse restart',
+        start: '-10% bottom',
+      },
+      onComplete: () => {
+        videoRef.current.play();
+      }
+    })
