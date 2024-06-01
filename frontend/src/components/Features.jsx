83d57b99ee5e59e8ffa4b774ae48ccb700e1697f
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-02 00:35:27 +0530

    features

---
+++ frontend/src/components/Features.jsx
@@ -0,0 +1,5 @@
+import { useGSAP } from '@gsap/react'
+import React, { useRef } from 'react'
+import { animateWithGsap } from '../utils/animations';
+import { explore1Img, explore2Img, exploreVideo } from '../utils';
+import gsap from 'gsap';
