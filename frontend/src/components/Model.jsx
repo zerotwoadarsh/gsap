2f4e5e8aaecf51ea149956bcb35431bc2156a9e6
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-03 20:45:41 +0530

    models

--- frontend/src/components/Model.jsx
+++ frontend/src/components/Model.jsx
@@ -47,3 +47,14 @@ const Model = () => {
       })
     }
   }, [size])
+
+  useGSAP(() => {
+    gsap.to('#heading', { y: 0, opacity: 1 })
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
