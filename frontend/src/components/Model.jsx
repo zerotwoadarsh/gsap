94306f8c76516aee2d585a52ecdb8b1b4364a555
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-03 20:31:16 +0530

    models

---
+++ frontend/src/components/Model.jsx
@@ -0,0 +1,6 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
+import { yellowImg } from "../utils";
+
