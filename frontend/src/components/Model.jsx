482c2bfb29aaea2ec889a6f11b3a1ccc10f4f931
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-03 20:37:47 +0530

    models

--- frontend/src/components/Model.jsx
+++ frontend/src/components/Model.jsx
@@ -9,3 +9,12 @@ import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
 import { animateWithGsapTimeline } from "../utils/animations";
+
+const Model = () => {
+  const [size, setSize] = useState('small');
+  const [model, setModel] = useState({
+    title: 'iPhone 15 Pro in Natural Titanium',
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg,
+  })
+
