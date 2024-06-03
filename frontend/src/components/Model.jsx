b45468f166070bcacb25b6b98af450ba8e363e4e
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-03 20:36:12 +0530

    models

--- frontend/src/components/Model.jsx
+++ frontend/src/components/Model.jsx
@@ -4,3 +4,8 @@ import ModelView from "./ModelView";
 import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 
+import * as THREE from 'three';
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
