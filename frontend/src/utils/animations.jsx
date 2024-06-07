d506b1bca5cc87b80ec9da8edc4309d6813094ad
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-07 20:44:27 +0530

    animations

--- frontend/src/utils/animations.jsx
+++ frontend/src/utils/animations.jsx
@@ -14,3 +14,10 @@ export const animateWithGsap = (target, animationProps, scrollProps) => {
     }
   })
 }
+
+export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: 'power2.inOut'
+  })
