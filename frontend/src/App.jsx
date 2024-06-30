d09f44da359cec86cf5369b7d66eaeb82756addc
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-06-30 21:59:48 +0530

    feat: Add new components to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 import Navbar from './components/Navbar'
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <div className=''>
-    <Navbar />
-    <Hero />
-    <Highlights />
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </div>
   )
 }
