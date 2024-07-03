fc0a42ec3b9528d6d513f15dcfaf42bd968fca0a
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-07-03 23:09:38 +0530

    chore: Refactor HowItWorks component and add animations

--- frontend/src/components/HowItWorks.jsx
+++ frontend/src/components/HowItWorks.jsx
@@ -4,3 +4,27 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
 import { animateWithGsap } from '../utils/animations';
 
+const HowItWorks = () => {
+  const videoRef = useRef();
+
+  useGSAP(() => {
+    gsap.from('#chip', {
+      scrollTrigger: {
+        trigger: '#chip',
+        start: '20% bottom'
+      },
+      opacity: 0,
+      scale: 2,
+      duration: 2,
+      ease: 'power2.inOut'
+    })
+
+    animateWithGsap('.g_fadeIn', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: 'power2.inOut'
+    })
+  }, []);
+
+  
