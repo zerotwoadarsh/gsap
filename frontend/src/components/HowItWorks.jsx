70ea69b7c06e2aea710056aee5d5b0418b6e85bc
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-07-03 23:10:11 +0530

    Refactor HowItWorks component and add animations

--- frontend/src/components/HowItWorks.jsx
+++ frontend/src/components/HowItWorks.jsx
@@ -27,4 +27,21 @@ const HowItWorks = () => {
     })
   }, []);
 
-  
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <div id="chip" className="flex-center w-full my-20">
+          <img src={chipImg} alt="chip" width={180} height={180} />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="hiw-title">
+            A17 Pro chip.
+            <br /> A monster win for gaming.
+          </h2>
+
+          <p className="hiw-subtitle">
+            It's here. The biggest redesign in the history of Apple GPUs.
+          </p>
+        </div>
+
